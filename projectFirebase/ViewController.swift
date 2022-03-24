19346d41e9efd9ffed78b307a56357c563bde9db
Francorod <francorodios>
2022-03-24 16:08:14 -0400

    First project connecting Swift with Firestore

--- projectFirebase/ViewController.swift
+++ projectFirebase/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var quoteLabel: UILabel!
+    @IBOutlet weak var quoteTextField: UITextField!
+    @IBOutlet weak var authorTextField: UITextField!
+    
+    var docRef: DocumentReference!
+    
+    @IBAction func saveTapped(_ sender: UIButton) {
+        guard  let quoteText = quoteTextField.text, !quoteText.isEmpty else { return }
+        guard let quoteAuthor = authorTextField.text, !quoteAuthor.isEmpty else { return }
+        let dataToSave: [String: Any] = ["quote": quoteText, "author": quoteAuthor]
+        docRef.setData(dataToSave) { ( error ) in
+            if let error = error {
+                print("Oh no! Got an error \(error.localizedDescription)" )
+            }else {
+                print("Data has been saved")
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        docRef = Firestore.firestore().collection("sampleData").document("inspiration")
+        
     }
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
 
 }
 
