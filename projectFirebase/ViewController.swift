98f6892ddde0fae67343692b19533cd40aca2e5f
Francorod <francorodios>
2022-03-24 12:48:59 -0400

    Initial Commit

---
+++ projectFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  projectFirebase
+//
+//  Created by Franco Rodrigues on 3/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
